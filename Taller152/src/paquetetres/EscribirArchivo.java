4a02b490bc5d42e0b205dbffecdfadadb816deea
Kriss Bustamante <krissbustamante25@gmail.com>
2021-02-02 23:43:02 -0500

    Entrega

--- Taller152/src/paquetetres/EscribirArchivo.java
+++ Taller152/src/paquetetres/EscribirArchivo.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package paquetetres;
-
+import java.util.Formatter;
 /**
  *
  * @author reroes
  */
 public class EscribirArchivo {
     
+   public static void agregarLibros(String valor) {
+        try {
+            Formatter salida = new Formatter("data/informacion3.txt");
+            salida.format("%s\n", valor);
+            salida.close();
+
+        } catch (Exception e) {
+            System.err.println("Error al crear el archivo.");
+            System.exit(1);
+
+        }
+
+    }
+    
 }
+
